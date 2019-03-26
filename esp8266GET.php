9eee78bb62aa0ecb80b0ca2db4054a2cbd45e7db
Rafael Zapata Grau <razapata@gmail.com>
2019-03-25 23:14:25 -0500

    Estructura de carpeta, Mejoras GET, POST y comentario form.html

--- esp8266GET.php
+++ esp8266GET.php
@@ -1,20 +1,16 @@
 <?php
-  $Temp=$_POST["temperature"];
+  $Temp=$_GET["temperature"];
   //create a variable that will receive the temperature
-  $Humidity=$_POST["humidity"];
+  $Humidity=$_GET["humidity"];
   //create a variable tat will receive the huminity
   $Write="<p>Temperature : " . $Temp . " Celcius </p>" . "<p>Humidity : " . $Humidity . " % </p>";
   //create an HTML file to display the data within
-
-//file_put_contents('sensor1.html',$Write);
-
-	$fileStatus=file_put_contents('/ResultPrueba/MetodoPOST.html',$Write);
+//file_put_contents('sensor.html',$Write);
+ $fileStatus=file_put_contents('/ResultPrueba/MetodoGET.html',$Write);
 	if ($fileStatus != false ) {
 		echo("Registro adicionado Correctamente.");
 	} else {
 		echo("Falla, no se proceso el archivo.");
 	}
-
-
 
  ?>
